2776482bec6ad0e3a2600f2f1d04296062bbd88e
serviktor050 <mr.s-gagulin@yandex.ru>
2020-01-19 17:58:55 +0300

    Выполнена отрисовка поля

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,8 +1,8 @@
 const themes = {
-  prairie: 'prairie',
-  desert: 'desert',
-  arctic: 'arctic',
-  mountain: 'mountain',
+  prairie: { level: 1, name: 'prairie'},
+  desert: { level: 2, name: 'desert'},
+  arctic: { level: 3, name: 'arctic'},
+  mountain: { level: 4, name: 'mountain'},
 };
 
 export default themes;
