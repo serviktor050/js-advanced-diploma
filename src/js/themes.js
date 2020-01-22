f131e2b98062a4cbc1189a36b067a5a81d3e1800
serviktor050 <mr.s-gagulin@yandex.ru>
2020-01-22 21:45:06 +0300

    Доработки по рабочему окружению и корректировка тем

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,8 +1,8 @@
 const themes = {
-  prairie: { level: 1, name: 'prairie'},
-  desert: { level: 2, name: 'desert'},
-  arctic: { level: 3, name: 'arctic'},
-  mountain: { level: 4, name: 'mountain'},
+  prairie: { level: 1, name: 'prairie' },
+  desert: { level: 2, name: 'desert' },
+  arctic: { level: 3, name: 'arctic' },
+  mountain: { level: 4, name: 'mountain' },
 };
 
 export default themes;
