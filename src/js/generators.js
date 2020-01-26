7c92862b893b8ba988c5c4d9de50052ac3896ee1
serviktor050 <mr.s-gagulin@yandex.ru>
2020-01-26 13:23:22 +0300

    Реализована ренерация команд

--- src/js/generators.js
+++ src/js/generators.js
@@ -7,8 +7,20 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  while (true) {
+    const typePlayer = Math.floor(Math.random() * allowedTypes.length);
+    const level = Math.floor(Math.random() * maxLevel) + 1;
+    yield new allowedTypes[typePlayer](level);
+  }
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const hero = characterGenerator(allowedTypes, maxLevel);
+  const userTeam = [];
+
+  for (let i = 0; i < characterCount; i += 1) {
+    userTeam.push(hero.next().value);
+  }
+  return userTeam;
 }
